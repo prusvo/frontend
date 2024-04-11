5bcd1d47f69b6962cb2af199ddc7ce7707d52657
Vitaliy Prus <128804224+prusvo@users.noreply.github.com>
2024-04-11 18:31:45 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
 
     
     useEffect(() => {
-        axios.get('http://localhost:5001/auth/verify')
+        axios.get('http://3.122.206.81:5001/auth/verify')
         .then(res => {
             if(res.data.status) {
                 
